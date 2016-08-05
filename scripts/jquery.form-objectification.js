3dd4ec444cf409d8de093408a63585f61955015e
MartinCamen <camen@camensky.se>
2016-08-05 10:35:51 +0200

    Wobbly lines

--- scripts/jquery.form-objectification.js
+++ scripts/jquery.form-objectification.js
@@ -3,14 +3,14 @@
     $.fn.objectify = function( options ) {
 
         var settings = $.extend({
-            'form' 	   : 'form',
+            'form' : 'form',
             'selector' : 'name'
         }, options);
 
-        var dataObject 		 = {},
+        var dataObject = {},
             allowedSelectors = ['name', 'id'],
-            inputs 	   		 = $(settings.form + ' :input')
-                .not(':input[type=button], :input[type=submit], :input[type=reset], :button');
+            inputs = $(settings.form + ' :input')
+                     .not(':input[type=button], :input[type=submit], :input[type=reset], :button');
 
         settings.selector = $.inArray(settings.selector, allowedSelectors) < 0 ? 'name' : settings.selector;
 
