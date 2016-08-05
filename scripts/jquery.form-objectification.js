d1e16cd59c6836b2c6fea3665289a8a4c8099199
MartinCamen <camen@camensky.se>
2016-08-05 10:34:33 +0200

    Initial commit

---
+++ scripts/jquery.form-objectification.js
@@ -0,0 +1,24 @@
+(function($) {
+
+    $.fn.objectify = function( options ) {
+
+        var settings = $.extend({
+            'form' 	   : 'form',
+            'selector' : 'name'
+        }, options);
+
+        var dataObject 		 = {},
+            allowedSelectors = ['name', 'id'],
+            inputs 	   		 = $(settings.form + ' :input')
+                .not(':input[type=button], :input[type=submit], :input[type=reset], :button');
+
+        settings.selector = $.inArray(settings.selector, allowedSelectors) < 0 ? 'name' : settings.selector;
+
+        $.each(inputs, function(i) {
+            dataObject[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
+        });
+
+        return dataObject;
+    };
+
+}(jQuery));
