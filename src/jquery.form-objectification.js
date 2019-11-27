f330657fdd009d2106ac7ef66efc1d75786bcfae
MartinCamen <MartinCamen@users.noreply.github.com>
2019-11-27 23:37:46 +0100

    Update jquery.form-objectification.js
    Remove spaces

--- src/jquery.form-objectification.js
+++ src/jquery.form-objectification.js
@@ -1,5 +1,4 @@
 (function($) {
-
     $.fn.objectifyForm = function( options ) {
 
         var settings = $.extend({
@@ -9,64 +8,61 @@
             'checkboxesData' : 'boolean'
         }, options);
 
-        var dataObject       = {},
+        var data             = {},
             allowedSelectors = ['name', 'id'],
             inputs           = $(this).find(':input').not(':input[type=button], :input[type=submit], :input[type=reset], :button');
 
-        if ( typeof settings.before != 'undefined' && typeof settings.before == 'function' ) {
-            settings.before.call(this, settings, dataObject);
+        if (typeof settings.before != 'undefined' && typeof settings.before == 'function') {
+            settings.before.call(this, settings, data);
         }
 
-        settings.selector = $.inArray(settings.selector, allowedSelectors) < 0 ? 'name' : settings.selector;
+        settings.selector = $.inArray(settings.selector, allowedSelectors) > -1 ? settings.selector : 'name';
 
-        $.each( inputs, function(i) {
+        $.each(inputs, function(i) {
             var include = true;
 
-            if ( settings.exclude.length ) {
-                if ( $.inArray($(inputs[i]).attr('name'), settings.exclude) > -1 ) {
+            if (settings.exclude.length) {
+                if ($.inArray($(inputs[i]).attr('name'), settings.exclude) > -1) {
                     include = false;
-                }
-                else {
+                } else {
                     $.each(settings.exclude, function (key, value) {
-                        if ( ( ( value.substring(0, 1) == '#' ) && ( '#' + $(inputs[i]).attr('id') == value ) ) ||
-                            ( ( value.substring(0, 1) == '.' ) && ( $(inputs[i]).hasClass(value.replace('.', '')) ) ) ) {
+                        if (
+                            ((value.substring(0, 1) == '#') && ('#' + $(inputs[i]).attr('id') == value )) ||
+                            ((value.substring(0, 1) == '.') && ($(inputs[i]).hasClass(value.replace('.', ''))))
+                           ) {
                             include = false;
                         }
                     });
                 }
             }
 
-            if ( include ) {
-                if ( $(inputs[i]).is(':checkbox') ) {
-                    if ( settings.checkboxesAll ) {
-                        if ( settings.checkboxesData === 'boolean' ) {
-                            dataObject[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).is(':checked') ? 1 : 0;
+            if (include) {
+                if ($(inputs[i]).is(':checkbox')) {
+                    if (settings.checkboxesAll) {
+                        if (settings.checkboxesData === 'boolean') {
+                            data[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).is(':checked') ? 1 : 0;
+                        } else {
+                            data[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
                         }
-                        else {
-                            dataObject[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
-                        }
+                    } else if ($(inputs[i]).is(':checked')) {
+                        data[$(inputs[i]).attr(settings.selector)] = 1;
                     }
-                    else if ( $(inputs[i]).is(':checked') ) {
-                        dataObject[$(inputs[i]).attr(settings.selector)] = 1;
+                } else if ($(inputs[i]).is(':radio')) {
+                    if ($(inputs[i]).is(':checked')) {
+                        data[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
                     }
-                }
-                else if ( $(inputs[i]).is(':radio') ) {
-                    if ( $(inputs[i]).is(':checked') ) {
-                        dataObject[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
-                    }
-                }
-                else {
-                    dataObject[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
+                } else {
+                    data[$(inputs[i]).attr(settings.selector)] = $(inputs[i]).val();
                 }
             }
 
         });
 
-        if ( typeof settings.after != 'undefined' && typeof settings.after == 'function' ) {
-            settings.after.call(this, settings, dataObject);
+        if (typeof settings.after != 'undefined' && typeof settings.after == 'function') {
+            settings.after.call(this, settings, data);
         }
 
-        return dataObject;
+        return data;
     };
 
 }(jQuery));
